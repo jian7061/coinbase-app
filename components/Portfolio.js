526ef6708f7a56a2a23c0978a0297011ca885a87
Jian Kim <jian7061@gmail.com>
2022-07-28 20:10:45 +0900

    Add Portfolio Component

---
+++ components/Portfolio.js
@@ -0,0 +1,88 @@
+import React from "react";
+import styled from "styled-components";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import Coin from "./Coin";
+import { coins } from "../public/static/coins";
+
+const Portfolio = () => {
+  return (
+    <Wrapper>
+      <PortfolioTable>
+        <TableItem>
+          <Title>Your Assets</Title>
+        </TableItem>
+        <Divider />
+        <Table>
+          <TableItem>
+            <TableRow>
+              <div style={{ flex: 3 }}>Balanc e</div>
+              <div style={{ flex: 2 }}>Name</div>
+              <div style={{ flex: 1 }}>Price</div>
+              <div style={{ flex: 1 }}>Allocation</div>
+              <div style={{ flex: 0 }}>
+                <BsThreeDotsVertical />
+              </div>
+            </TableRow>
+          </TableItem>
+          <Divider />
+          <div>
+            {coins.map((coin) => (
+              // eslint-disable-next-line react/jsx-key
+              <div>
+                <Coin coin={coin} />
+                <h2>{coin.name}</h2>
+                <Divider />
+              </div>
+            ))}
+          </div>
+        </Table>
+      </PortfolioTable>
+    </Wrapper>
+  );
+};
+
+export default Portfolio;
+
+const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  max-width: 1000px;
+  padding: 2rem 1rem;
+`;
+
+const PortfolioTable = styled.div`
+  margin-top: 1rem;
+  border: 1px solid #282b2f;
+`;
+
+const Table = styled.div`
+  width: 100%;
+`;
+
+const TableRow = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  & > th {
+    text-align: left;
+  }
+`;
+
+const TableItem = styled.div`
+  border-bottom: 1px solid #282b2f;
+`;
+
+const Divider = styled.div`
+  border-bottom: 1px solid #282b2f;
+`;
+
+const Title = styled.div`
+  font-size: 1.5rem;
+  font-weight: 600;
+`;
